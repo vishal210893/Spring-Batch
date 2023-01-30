0aabb0b20f17b62eb827dd6caef307ecda95d6c5
Vishal Kumar <64398452+vishal-kumar-solum@users.noreply.github.com>
2023-01-30 18:58:53 +0530

    First commit

---
+++ src/test/java/com/aims/solum/spring_batch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aims.solum.spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
