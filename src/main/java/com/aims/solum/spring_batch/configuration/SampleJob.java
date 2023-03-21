95ca1ae34e3a5a1643d903a6986938b2dd02f156
Vishal Kumar <64398452+vishal-kumar-solum@users.noreply.github.com>
2023-03-21 15:04:52 +0900

    file item reader

--- src/main/java/com/aims/solum/spring_batch/configuration/SampleJob.java
+++ src/main/java/com/aims/solum/spring_batch/configuration/SampleJob.java
@@ -11,9 +11,13 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
+import org.springframework.batch.item.json.JsonFileItemWriter;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -49,27 +53,42 @@ public class SampleJob {
 				.<StudentCsv, StudentCsv>chunk(3, transactionManager)
 				.reader(flatFileItemReader())
 				//.processor(firstItemProcessor)
-				.writer(fileItemWriter)
+				//.writer(fileItemWriter)
+				.writer(jsonFileItemWriter())
 				.build();
 		return fileStep;
 	}
 
-	public FlatFileItemReader<StudentCsv> flatFileItemReader() throws FileNotFoundException {
-		FlatFileItemReader<StudentCsv> flatFileItemReader = new FlatFileItemReader<>();
-		flatFileItemReader.setResource(new FileSystemResource(ResourceUtils.getFile("classpath:students.csv")));
+	private FlatFileItemReader<StudentCsv> flatFileItemReader() throws FileNotFoundException {
 
 		final DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
 		delimitedLineTokenizer.setNames("ID", "First Name", "Last Name", "Email");
+
 		final BeanWrapperFieldSetMapper<StudentCsv> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapper<>();
 		beanWrapperFieldSetMapper.setTargetType(StudentCsv.class);
 
-		final DefaultLineMapper<StudentCsv> lineMapper = new DefaultLineMapper<>();
-		lineMapper.setLineTokenizer(delimitedLineTokenizer);
-		lineMapper.setFieldSetMapper(beanWrapperFieldSetMapper);
+		//final DefaultLineMapper<StudentCsv> lineMapper = new DefaultLineMapper<>();
+		//lineMapper.setLineTokenizer(delimitedLineTokenizer);
+		//lineMapper.setFieldSetMapper(beanWrapperFieldSetMapper);
 
-		flatFileItemReader.setLineMapper(lineMapper);
-		flatFileItemReader.setLinesToSkip(1);
+		final FlatFileItemReader<StudentCsv> flatFileItemReader = new FlatFileItemReaderBuilder<StudentCsv>()
+				.resource(new FileSystemResource(ResourceUtils.getFile("classpath:students.csv")))
+				//.lineMapper(lineMapper)
+				.lineTokenizer(delimitedLineTokenizer)
+				.fieldSetMapper(beanWrapperFieldSetMapper)
+				.linesToSkip(1)
+				.name("flatfile csv reader")
+				.build();
 		return flatFileItemReader;
+	}
+
+	private JsonFileItemWriter<StudentCsv> jsonFileItemWriter() {
+		final JsonFileItemWriter<StudentCsv> jsonFileItemWriter = new JsonFileItemWriterBuilder<StudentCsv>()
+				.resource(new FileSystemResource("D:\\Spring Batch\\src\\main\\resources\\students.json"))
+				.jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+				.name("csvJsonFileItemWriter")
+				.build();
+		return jsonFileItemWriter;
 	}
 
 }
